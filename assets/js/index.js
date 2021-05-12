59254e6ca27fa41b08cf6be6b6f1005f09fe3e34
Francisco <f.nav.del@gmail.com>
2021-05-12 15:44:51 -0400

    smooth scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
+// smooth scroll
+$(document).ready(function() {
+    $("a").click(function(event) {
+        event.preventDefault();
+
+        var smooth = this.hash;
+
+        $("html").animate({
+                scrollTop: $(smooth).offset().top,
+            },
+            800
+        );
+    });
+});
+
+
+// Activiacion de los toogle de bootstrap
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
